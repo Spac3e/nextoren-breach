3480259084704b32764101b1c1b09de18e3a861b
Artem <100233737+Spac3e@users.noreply.github.com>
2023-05-02 20:20:29 +0300

    Убираем icon16

--- other/addons/[chat]chatbox/lua/chatbox_emoticons.lua
+++ other/addons/[chat]chatbox/lua/chatbox_emoticons.lua
@@ -1,22 +1,10 @@
---[[
-Server Name: Breach 2.6.0 [Alpha]
-Server IP:   94.26.255.7:27415
-File Path:   addons/[chat]_sh_lounge_chatbox/lua/chatbox_emoticons.lua
-		 __        __              __             ____     _                ____                __             __         
-   _____/ /_____  / /__  ____     / /_  __  __   / __/____(_)__  ____  ____/ / /_  __     _____/ /____  ____ _/ /__  _____
-  / ___/ __/ __ \/ / _ \/ __ \   / __ \/ / / /  / /_/ ___/ / _ \/ __ \/ __  / / / / /    / ___/ __/ _ \/ __ `/ / _ \/ ___/
- (__  ) /_/ /_/ / /  __/ / / /  / /_/ / /_/ /  / __/ /  / /  __/ / / / /_/ / / /_/ /    (__  ) /_/  __/ /_/ / /  __/ /    
-/____/\__/\____/_/\___/_/ /_/  /_.___/\__, /  /_/ /_/  /_/\___/_/ /_/\__,_/_/\__, /____/____/\__/\___/\__,_/_/\___/_/     
-                                     /____/                                 /____/_____/                                  
---]]
-
 /**
 * Derma Emoticons 
 **/
 
 -- Enable Derma emoticons?
 -- You can see the full list here: http://www.famfamfam.com/lab/icons/silk/previews/index_abc.png
-LOUNGE_CHAT.EnableDermaEmoticons = true
+LOUNGE_CHAT.EnableDermaEmoticons = false
 
 -- Restrict Derma emoticons?
 -- You can configure the restrictions in the "DermaEmoticonsRestrictions" option.
