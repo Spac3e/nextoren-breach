2fa30a24faf3ff4d4e26aa623834f8ce00a00381
Artem <100233737+Spac3e@users.noreply.github.com>
2023-05-02 18:45:55 +0300

    whoops nasral

--- breach/gamemode/modules/sh_classes.lua
+++ breach/gamemode/modules/sh_classes.lua
@@ -118,7 +118,7 @@ CLASSDMODELS = {
 }
 	
 	CLASSDMODELSMALES = {
-	"models/cultist/humans/class_d/class_d.mdl.mdl",
+	"models/cultist/humans/class_d/class_d.mdl",
 	}
 	ALLCLASSES = {
 	
@@ -199,6 +199,9 @@ CLASSDMODELS = {
 				 customcheck = nil,
 	
 				 flashlight = false,
+
+				 
+				 ability = {"Thief", 45, "Украдите у человека предмет который он держит на данный момент!", "nextoren/gui/special_abilities/ability_thief.png", false},
 	
 				 max = 3
 	
@@ -208,7 +211,7 @@ CLASSDMODELS = {
 	
 				 team = TEAM_GOC,
 	
-				 weapons = {"br_holster", "weapon_flashlight", "class_d_zatoshka", "weapon_ai_scanner"},
+				 weapons = {"br_holster", "weapon_flashlight", "item_knife"},
 	
 				 showweapons = {"Фонарик", "Заточка", "Сканер"},
 	
@@ -446,7 +449,7 @@ CLASSDMODELS = {
 	
 				 walkspeed = 0.98,
 	
-				 weapons = {"big_nigga_hands"},
+				 weapons = {"br_holster"},
 	
 				 runspeed = 1.02,
 	
@@ -1031,7 +1034,7 @@ CLASSDMODELS = {
 	
 				 importancelevel = 3,
 
-				 bodygroups = "0101"
+				 bodygroups = "11101"
 	
 				},
 	
@@ -1072,6 +1075,10 @@ CLASSDMODELS = {
 				 max = 1,
 	
 				 sorting = 4,
+
+				 ability = {"Anti-SCP Turret", 1, "Вы ставите турель.", "nextoren/gui/ability/engineer_turret.png", true},
+
+				 ability_max = 1,
 	
 				 importancelevel = 3
 	
@@ -1328,6 +1335,8 @@ CLASSDMODELS = {
 				 flashlight = true,
 	
 				 level = 16,
+
+				 ability = {"Cameras Scan", 120, "None Provided", "nextoren/gui/special_abilities/special_ntf_commander.png", false},
 	
 				 max = 1
 	
@@ -1522,6 +1531,8 @@ CLASSDMODELS = {
 				 flashlight = true,
 	
 				 level = 14,
+
+				 ability = {"Cloak", 80, "None Provided", "nextoren/gui/special_abilities/special_goc_invisibility.png", false},
 	
 				 max = 1
 	
@@ -1598,6 +1609,8 @@ CLASSDMODELS = {
 				 flashlight = true,
 	
 				 level = 7,
+
+				 ability = {"Intelligence", 45, "None Provided", "nextoren/gui/special_abilities/special_fbi_commander.png", false},
 	
 				 max = 1
 	
@@ -1636,6 +1649,10 @@ CLASSDMODELS = {
 				 flashlight = true,
 	
 				 level = 5,
+
+				 ability = {"Portal", 90, "None Provided", "nextoren/gui/special_abilities/special_dz_portal.png", false},
+
+				 ability_max = 1,
 	
 				 max = 3
 	
@@ -1684,6 +1701,8 @@ CLASSDMODELS = {
 				 flashlight = false,
 	
 				 level = 800,
+
+				 ability = {"AOE Heal", 75, "Исцеляет людей в некотором радиусе от вас.", "nextoren/gui/special_abilities/special_heal.png", false},
 	
 				 max = 1
 	
@@ -1700,8 +1719,6 @@ CLASSDMODELS = {
 				 showweapons = {"Специальная способность - Обнаружение всех живых СЦП объектов в течении 10 секунд"},
 	
 				 ammo = {{"Pistol", 20}},
-	
-	
 	
 				 health = 110,
 	
@@ -1722,6 +1739,8 @@ CLASSDMODELS = {
 				 flashlight = false,
 	
 				 level = 0,
+
+				 ability = {"Special Vision", 60, "Вы начинаете видеть SCP через стены.", "nextoren/gui/special_abilities/spy_chaos_focus.png", false},
 	
 				 max = 1
 	
@@ -1760,6 +1779,8 @@ CLASSDMODELS = {
 				 flashlight = false,
 	
 				 level = 8,
+
+				 ability = {"Slow", 85, "Замедляет SCP в некотором радиусе от вас.", "nextoren/gui/special_abilities/special_slow.png", false},
 	
 				 max = 1
 	
@@ -1798,6 +1819,8 @@ CLASSDMODELS = {
 				 flashlight = false,
 	
 				 level = 24,
+
+				 ability = {"Special Shield", 80, "Вы создаёте вокруг себя щит, не пропускающий SCP-объекты.", "nextoren/gui/special_abilities/special_shield.png", false},
 	
 				 max = 1
 	
@@ -1876,6 +1899,10 @@ CLASSDMODELS = {
 				 flashlight = false,
 	
 				 level = 22,
+
+				 ability = {"Anti-SCP mine", 3, "Вы ставите Anti-SCP мину.", "nextoren/gui/special_abilities/special_mine.png", true},
+
+				 ability_max = 3,
 	
 				 max = 1
 	
@@ -1914,6 +1941,8 @@ CLASSDMODELS = {
 				 flashlight = false,
 	
 				 level = 19,
+
+				 ability = {"Damage Boost", 75, "Вы увеличиваете урон людям в радиусе, урон по SCP.", "nextoren/gui/special_abilities/special_debuffscp.png", false},
 	
 				 max = 1
 	
