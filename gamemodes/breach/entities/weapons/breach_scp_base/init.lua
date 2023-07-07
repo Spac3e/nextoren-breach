90ec4a658c1749fc8e41d736a2e89d238e0e2cba
Artem <100233737+Spac3e@users.noreply.github.com>
2023-07-07 10:20:01 +0300

    Убрал всякую фигню

--- gamemodes/breach/entities/weapons/breach_scp_base/init.lua
+++ gamemodes/breach/entities/weapons/breach_scp_base/init.lua
@@ -3,8 +3,5 @@ AddCSLuaFile("shared.lua")
 include("shared.lua")
 
 util.AddNetworkString("Ability_ClientsideCooldown")
-util.AddNetworkString("SHAKY_SetForcedAnimSync")
-util.AddNetworkString("SHAKY_EndForcedAnimSync")
 util.AddNetworkString("GestureClientNetworking")
 util.AddNetworkString("ForbidTalant")
-util.AddNetworkString("ThirdPersonCutscene")
